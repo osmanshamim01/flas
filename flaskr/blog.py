1c37ef83b18d78bad866408bb4036bf7e09cb2c8
Shamim Osman <osmansystem@outlook.com>
2024-03-19 11:04:55 +0600

    last update

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -72,7 +72,7 @@ def addevent():
 
 def get_event(id, check_author=True):
     event = get_db().execute(
-        'SELECT * '
+        'SELECT id,  '
         ' FROM events '
         ' WHERE id = ?',
         (id,)
@@ -95,6 +95,6 @@ def eventmanage():
 def eventdelete(id):
     get_event(id)
     db = get_db()
-    db.execute('DELETE FROM post WHERE id = ?', (id,))
+    db.execute('DELETE FROM event WHERE id = ?', (id,))
     db.commit()
     return redirect(url_for('blog.index'))
